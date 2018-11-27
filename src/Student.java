c77ac5aa790110d076148389f7d44da1145026b5
brendan_ho <brendan_ho@student.wayland.k12.ma.us>
2018-11-27 08:14:20 -0500

    oyoyo

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
 public class Student {
-
     private int id, grade;
+    ArrayList<Class> requests = new ArrayList<Class>();
+    ArrayList<Class> schedule = new ArrayList<Class>();
+
     //draw physical  array for schedule;
     //list of course reqs;
-
-
     public Student(){
+    }
 
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int i) {
+        grade = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int i) {
+        id = i;
     }
 
 }
