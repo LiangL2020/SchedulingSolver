6d25a5bb12b07dd269536640bc493f9f254cea94
uma_paithankar <uma_paithankar@student.wayland.k12.ma.us>
2018-11-27 07:52:55 -0500

    classes

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,9 +1,23 @@
 public class Classroom {
     private int id, wingId, maxStudents;
+    private boolean open; //true is classrm available, false is unavailable.in use
+    private int idOfClass;
 
     public Classroom(){
+        this.id = id;
+        this.wingId = wingId;
+        this.maxStudents = maxStudents;
+        this.open = open;
+        this.idOfClass = idOfClass;
+
 
     }
 
     //.getrmnumber
+
+    public
+
+
+
+
 }
