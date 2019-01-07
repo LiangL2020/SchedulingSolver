ec8946c5edfb58b3a3e18b5f50a0a4991ee4035e
teamcaptainanthony <anthony789xue@gmail.com>
2019-01-07 10:18:01 -0500

    data.txt

--- src/Population.java
+++ src/Population.java
@@ -5,13 +5,13 @@ public class Population {
     //genetic algorithm population
     //takes in masterschedule chromosomes adds them to population
     private ArrayList<MasterSchedule> members;
-    private String goal;
+    //private String goal;
     private int size;
     private int generationNumber;
 
     public Population(int size, String goal){
         this.members = new ArrayList<MasterSchedule>();
-        this.goal = goal;
+     //   this.goal = goal;
         this.size = size;
         this.generationNumber = generationNumber;
         while(members.size()<size){
@@ -19,4 +19,21 @@ public class Population {
             members.add(masterSchedule);
         }
     }
+
+  /*  public void sort(){
+       Collections.sort(members, new Comparator<MasterSchedule>() {
+            @Override
+            public int compare(MasterSchedule a, MasterSchedule b){
+
+            }
+        }
+
+    }
+    */
+
+  /*public boolean generation() {
+  }
+  */
+
+  }
 }
