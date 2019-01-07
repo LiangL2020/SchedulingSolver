f5cb63eaa258f9cdef5057bf864059d7cc838544
brendan_ho <brendan_ho@student.wayland.k12.ma.us>
2019-01-07 10:17:32 -0500

    MasterSchedule

--- src/MasterSchedule.java
+++ src/MasterSchedule.java
@@ -12,8 +12,6 @@ public class MasterSchedule {
 
         this.Periods = Periods;
 
-
-
     }
 
     public ArrayList[] RandomizeClasses() {
@@ -22,9 +20,37 @@ public class MasterSchedule {
 
         for (int i = 0; i < AllCourses.size(); i++) {
 
-            int random = (int)(Math.random()*8+1);
+            int bruh = (int)(Math.random()*8+1);
 
-            Schedule[random].add(AllCourses.get(i));
+            Schedule[bruh].add(AllCourses.get(i));
+
+        }
+
+        return Schedule;
+
+    }
+
+    public ArrayList<Course>[] MutateClasses(ArrayList<Course>[] Schedule) {
+
+        int probability = (int)(Math.random()*100);
+
+        if(probability < 20) {
+
+            int periodA = (int) (Math.random() * Schedule.length);  //rand period
+
+            int periodB = (int) (Math.random() * Schedule.length); //rand period
+
+            int classAIndex = (int) (Math.random() * Schedule[periodA].size());  //rand class from period
+
+            int classBIndex = (int) (Math.random() * Schedule[periodB].size());  //rand class from period
+
+            Course classA = Schedule[periodA].remove(classAIndex);
+
+            Course classB = Schedule[periodB].remove(classBIndex);
+
+            Schedule[periodA].add(classB);
+
+            Schedule[periodB].add(classA);
 
         }
 
